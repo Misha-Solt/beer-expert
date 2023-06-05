1e5322048e1a3a13ddfa0be8d7c27273153bf812
OksanaSherstiuk <moonka.moonka@gmail.com>
2023-06-05 15:18:46 +0200

    filter functions added

--- controllers/beerController.js
+++ controllers/beerController.js
@@ -2,7 +2,14 @@ import { StatusCodes } from 'http-status-codes'
 import mongoose from 'mongoose'
 
 import Brand from '../models/Brand.js'
+import Beer from '../models/Beer.js'
 
+/**
+ * List all Beers (without Brand Name)
+ * @param {*} req
+ * @param {*} res
+ * @returns
+ */
 export const listBeers = async (req, res) => {
   try {
     const brands = await Brand.find().exec()
@@ -12,6 +19,128 @@ export const listBeers = async (req, res) => {
     })
 
     return res.status(StatusCodes.OK).json(allBeers)
+  } catch (error) {
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: error.message })
+  }
+}
+
+/**
+ * list all Beers with Brand Name
+ * @param {*} req
+ * @param {*} res
+ * @returns
+ */
+export const listBeersWithBrandName = async (req, res) => {
+  try {
+    const brands = await Brand.find().exec()
+    let allBeers = []
+
+    brands.forEach((brand) => {
+      const brandName = brand.brandName
+      brand.beers.forEach((beer) => {
+        const fullBeerName = `${brandName} ${beer.beerName}`
+        const beerWithBrandName = {
+          ...beer.toObject(),
+          brandName: brandName,
+          fullBeerName: fullBeerName,
+        }
+        allBeers.push(beerWithBrandName)
+      })
+    })
+
+    return res.status(StatusCodes.OK).json(allBeers)
+  } catch (error) {
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: error.message })
+  }
+}
+
+/**
+ * Function to get Details about chosen beer
+ * @param {*} req
+ * @param {*} res
+ * @returns
+ */
+export const getBeerDetails = async (req, res) => {
+  const beerId = req.params.beerId
+
+  try {
+    const beer = await Brand.findOne({ 'beers._id': beerId })
+      .populate('beers')
+      .exec()
+    if (!beer) {
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ message: 'Beer not found' })
+    }
+
+    const selectedBeer = beer.beers.find(
+      (beer) => beer._id.toString() === beerId
+    )
+    const beerWithBrand = {
+      ...selectedBeer.toObject(),
+      brandName: beer.brandName,
+      brandDescription: beer.brandDescription,
+      mainAddress: beer.mainAddress,
+      url: beer.url,
+    }
+
+    return res.status(StatusCodes.OK).json(beerWithBrand)
+  } catch (error) {
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: error.message })
+  }
+}
+
+/**
+ * List all beers of One Brand (by Brand ID)
+ * @param {*} req
+ * @param {*} res
+ * @returns
+ */
+export const listBeersByBrand = async (req, res) => {
+  const brandId = req.params.brandId
+
+  try {
+    const brand = await Brand.findById(brandId).exec()
+    if (!brand) {
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ message: 'Brand not found' })
+    }
+
+    const beers = brand.beers
+    return res.status(StatusCodes.OK).json(beers)
+  } catch (error) {
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: error.message })
+  }
+}
+
+/**
+ * Filter all beers by beerType
+ * @param {*} req
+ * @param {*} res
+ * @returns
+ */
+export const filterBeersByType = async (req, res) => {
+  const beerType = req.query.beerType
+
+  try {
+    const brands = await Brand.find({ 'beers.beerType': beerType }).exec()
+    const beers = brands.reduce((result, brand) => {
+      const filteredBeers = brand.beers.filter(
+        (beer) => beer.beerType === beerType //Beer Type here
+      )
+      return [...result, ...filteredBeers]
+    }, [])
+
+    return res.status(StatusCodes.OK).json(beers)
   } catch (error) {
     res
       .status(StatusCodes.INTERNAL_SERVER_ERROR)
--- routes/beerRoutes.js
+++ routes/beerRoutes.js
@@ -1,7 +1,13 @@
 import express from 'express'
 
 //functions for listing beers imported from BEER controller
-import { listBeers } from '../controllers/beerController.js'
+import {
+  listBeers,
+  listBeersByBrand,
+  getBeerDetails,
+  listBeersWithBrandName,
+  filterBeersByType,
+} from '../controllers/beerController.js'
 
 //functions for creating and listing brands imported from BRAND controller
 import {
@@ -12,13 +18,25 @@ import {
 
 const beerRoutes = express.Router()
 
-//GET http://localhost:3001/api/beers
+//GET http://localhost:3001/api/beers-no-brand
 //list all beers
-beerRoutes.get('/beers', listBeers)
+beerRoutes.get('/beers-no-brand', listBeers)
+
+//GET http://localhost:3001/api/beers
+//list all beers with brand name
+beerRoutes.get('/beers', listBeersWithBrandName)
+
+//GET http://localhost:3001/api/beers/:beerId
+//list a chosen beer with Brand details and Beer details
+beerRoutes.get('/beers/:beerId', getBeerDetails)
 
 //GET http://localhost:3001/api/brands
 //list all Brands
 beerRoutes.get('/brands', listBeerBrands)
+
+//GET http://localhost:3001/api/beers-by-brand/:brandId
+//list all Beers by one Brand
+beerRoutes.get('/beers-by-brand/:brandId', listBeersByBrand)
 
 //POST http://localhost:3001/api/add-brand
 //add a new brand
@@ -27,5 +45,11 @@ beerRoutes.post('/add-brand', addBeerBrand)
 //PATCH http://localhost:3001/api/add-beer/:id
 //update Brand by Adding a new beer
 beerRoutes.patch('/add-beer/:id', addBeer)
+
+//Searching (filtering) Beer Routes
+
+// Find Beer by beerType
+//GET http://localhost:3001/api/beers/by-type/type?beerType=WEIZEN
+beerRoutes.get('/beers/by-type/type', filterBeersByType)
 
 export default beerRoutes
