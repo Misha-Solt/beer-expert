b2a1e5cae7d9ddf42fc1f380d8e91266f5badad7
Misha Solt <mikhail.us@gmail.com>
2023-06-07 09:05:15 +0200

    A little bit fron for main page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,17 @@
 import './App.css'
 import AddBeerBrand from './components/AddBeerBrand/AddBeerBrand'
 import ListAllBrands from './components/ListAllBrands/ListAllBrands'
+import MainLayout from './components/MainLayout/MainLayout'
 // import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
+      <MainLayout />
       <header>Beers adding tool will be here!</header>
+
       <AddBeerBrand />
       <ListAllBrands />
-
       {/* <Routes>
         <Route path="/addBeerBrand" element={<AddBeerBrand />} />
       </Routes> */}
--- client/src/components/MainLayout/MainLayout.jsx
+++ client/src/components/MainLayout/MainLayout.jsx
@@ -3,6 +3,13 @@ import styles from './mainLayout.module.css'
 const MainLayout = () => {
   return (
     <div className={styles.mainLayout}>
+      <header className={styles.header}>beer-expert</header>
+      <h1 className={styles.greeting}>Hi friend!</h1>
+      <h3 className={styles.question}>
+        Today is a great time for a <span className={styles.beer}>beer;)</span>{' '}
+        <br /> Isn't it?
+      </h3>
+      <button className={styles.button}>Absolutely!</button>
     </div>
   )
 }
